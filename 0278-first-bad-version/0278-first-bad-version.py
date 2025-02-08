d7f34f8fde95058830d1f724a72044a81f52fe13
Galaxy Rawat <gvpg223@gmail.com>
2025-02-08 12:29:18 +0530

    Time: 36 ms (65.41%), Space: 17.9 MB (23.79%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.py
+++ 0278-first-bad-version/0278-first-bad-version.py
@@ -1,19 +1,15 @@
-# The isBadVersion API is already defined for you.
-# def isBadVersion(version: int) -> bool:
-
-class BinarySearch:
-    def find_first_bad_version(self, n):
-        low, high = 1, n
-        bad_version = -1
-        while low <= high:
-            mid = low + (high - low) // 2
-            if isBadVersion(mid):
-                bad_version = mid
-                high = mid - 1
-            else:
-                low = mid + 1
-        return bad_version
 
 class Solution:
-    def firstBadVersion(self, n: int) -> int:
-        return BinarySearch().find_first_bad_version(n)
+    def firstBadVersion(self, n):
+        L = 1
+        R = n
+
+        while L < R:
+            M = (L + R) // 2
+            if (isBadVersion(M)):
+
+                R = M
+            else:
+                L = M + 1
+
+        return L
